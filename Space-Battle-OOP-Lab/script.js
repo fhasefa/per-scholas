d5ca50f63b79fe9c5c4f5ed16bf365a4c67ce339
fhasefa <119895043+fhasefa@users.noreply.github.com>
2023-01-31 16:56:26 -0500

    message

---
+++ Space-Battle-OOP-Lab/script.js
@@ -0,0 +1,80 @@
+class Ship {
+    constructor(name, hull, firepower, accuracy) {
+        this.name = name
+        this.hull = hull
+        this.firepower = firepower
+        this.accuracy = accuracy
+        this.alienShips = []
+    }
+
+    attack(targetShip) {
+        if (Math.random() < this.accuracy) {
+            targetShip.hull -= this.firepower
+            console.log(`${this.name} has successfully hit ${targetShip.name}!`)}
+        else {
+            console.log(`${this.name} missed ${targetShip.name}!`)
+        }
+    }
+
+    generateAlienShip(numOfShips) {
+        let counter = 0;
+            while (counter < numOfShips) {
+             const alienShip = new Ship('Alien Ship', Math.floor(Math.random() * (6 - 3) + 3), Math.floor(Math.random() * (4 - 2) + 2), Math.random() * (0.8 - 0.6) + 0.6);
+             counter++;
+             this.alienShips.push(alienShip)       
+        }
+    }
+
+}
+
+
+let ussAssembly = new Ship('USS Assembly', 20, 5, 0.7);
+let alienBattleFleet = new Ship
+
+alienBattleFleet.generateAlienShip(Math.floor(Math.random() * (12 - 6) + 6))
+
+ console.log(alienBattleFleet.alienShips)
+// console.log(ussAssembly)
+
+function playGame() {
+    let start = prompt('Enter "start" to play game...');
+    if (start.toLowerCase() === 'start') {
+        // loop through each alien ship
+        for (let i = 0; i < alienBattleFleet.alienShips.length; i++) {
+            //console.log(alienBattleFleet.alienShips[i].name + (i + 1));
+            let alien = alienBattleFleet.alienShips[i];
+            let nextMove = prompt('"Do you want to attack next ship or retreat?"');
+            while (alien.hull > 0 && ussAssembly.hull > 0 && nextMove.toLowerCase() === 'attack') {
+                ussAssembly.attack(alien);
+                if (alien.hull <= 0) {
+                    console.log("Alien ship" + (1+i) + " destroyed!");
+                    if (nextMove.toLowerCase() === 'attack') {
+                        break;
+                    }
+
+                    else if (nextMove.toLowerCase() === 'retreat') {
+                        return console.log("USS Assembly retreated! Game Over.");
+                    }
+                } else {
+                    alien.attack(ussAssembly);
+                }
+            }
+
+            if (nextMove.toLowerCase() === 'retreat') {
+                return console.log("USS Assembly retreated! Game Over.");
+            }
+
+            if (ussAssembly.hull <= 0) {
+                return console.log("USS Assembly destroyed! Game Over.");
+            }
+        }
+
+        if (ussAssembly.hull > 0) {
+            return console.log("All Alien Ships destroyed! You Win!");
+        }
+    }
+}
+
+
+
+playGame()
